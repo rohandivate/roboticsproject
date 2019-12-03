f9d7dbf2e7793cc464a48377f8c0eb24cab2a80a
Class Account <ee106a-aey@c111-2.local>
2019-12-02 16:54:15 -0800

    setup again

---
+++ build/realsense-ros/realsense2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa19/class/ee106a-aey/ros_workspaces/finalproject/src/realsense-ros/realsense2_description
+# Build directory: /home/cc/ee106a/fa19/class/ee106a-aey/ros_workspaces/finalproject/build/realsense-ros/realsense2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/lab4_cam/cmake/lab4_cam-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(lab4_cam_MESSAGE_FILES "")
+set(lab4_cam_SERVICE_FILES "/home/cc/ee106a/fa19/class/ee106a-aey/ros_workspaces/finalproject/src/lab4_cam/srv/ImageSrv.srv")
